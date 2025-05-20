bae46e37d8d0dc8fadf423e1948c3c525469a077
“Thomas <t.mejia@udea.edu.co>
2025-05-19 23:32:17 -0500

    Cargar estructura de datos primera parte

--- Desafio_2/Data.h
+++ Desafio_2/Data.h
@@ -7,34 +7,81 @@
 
 using namespace std;
 
-void CargarData(const string fileName, string** ptrK){
+void CargarMatriz(string fileName, string** ptrK, const unsigned int &filas, const unsigned int &columnas);
+string** CargarData(const unsigned int &filas, const unsigned int &columnas, string &name);
+
+string** CargarData(const unsigned int &filas, const unsigned int &columnas, string &name) {
     /*
     Sinopsis:
-        Funcion encargada de abrir un archivo y cargar la estructura de datos
+        Funcion encargada de crear una matriz parcialmente dinamica de strings
+    param:
+        -(const unsigned int)filas: variable que almacena la cantidad de filas que va tener la matriz
+        -(const unsigned int)columnas: cantidad de columnas de la matriz
+        -(string)name: nombre del archivo que contiene la data
+*/
+    string** Anfitrion = new string*[filas];
+    for (unsigned int i = 0; i < filas; i++) {
+        Anfitrion[i] = new string[columnas];
+    }
+    CargarMatriz(name, Anfitrion, filas, columnas);
+    return Anfitrion;
+}
+
+void CargarMatriz(const string fileName, string** ptrK, const unsigned int &filas, const unsigned int &columnas) {
+    /*
+    Sinopsis:
+        Funcion encargada de cargar la estructura de datos llenando una matriz dinamica
     Param:
-    -(const char)&fileName: Argumento que almacena la direccion de memoria de donde se almacena el nombre del archivo de la
-    base de datos.
-    -(string*)&ptrK: Apuntador a la direccion de memoria de un apuntador de apuntadores
-    */
-    ifstream Informacion;
-    try{
-        Informacion.open(fileName);
-        if(!Informacion.is_open()){
-            throw runtime_error("Error: No se pudo leer el archivo ");
-        }
-        string Line;
-        getline(Informacion, Line);
-        for(unsigned int i=0; i<Line.size(); i++){
-            if(Line[i]!=","){
+        -(const string)fileName: nombre del archivo
+        -(string**)ptrK: Apuntador la matriz
+        -(const unsigned int)filas: numero de filas de la matriz
+        -(const unsigned int)columnas: numero de columnas de la matriz
+*/
 
-            }
-        }
-   }
-
-    catch(const runtime_error ){
-        cout<<"Error: No se pudo leer el archivo";
+    ifstream Informacion(fileName);
+    if (!Informacion.is_open()) {
+        cout << "Error: No se pudo leer el archivo";
+        return;
     }
 
+    string temp = "";
+    unsigned int i = 0, j = 0;
+    char c;
+
+    while (Informacion.get(c)) {
+
+        if (c == ',') {
+            if (!temp.empty() && i < filas && j < columnas) {
+                ptrK[i][j] = temp;
+                temp = "";
+                ++j;
+                if (j == columnas) {
+                    j = 0;
+                    ++i;
+                }
+            }
+        } else if (c == '\n') {
+            if (!temp.empty() && i < filas && j < columnas) {
+                ptrK[i][j] = temp;
+                temp = "";
+                ++i;
+                j = 0;
+            }
+        } else {
+            temp += c;
+        }
+    }
+
+    if (!temp.empty() && i < filas && j < columnas) {
+        ptrK[i][j] = temp;
+    }
+
+    Informacion.close();
 }
+
+
+
+
+
 
 #endif // DATA_H
